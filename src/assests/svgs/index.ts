1ad45a75a7adf116e718db719b74232d94b56f2b
Ghfshtfh291294@git <viethungtrieuphong1994@gmail.com>
2025-07-15 08:13:50 +0700

    Add svg file

---
+++ src/assests/svgs/index.ts
@@ -0,0 +1,6 @@
+import CartIcon from './CartIcon.svg';
+import HomeIcon from './HomeIcon.svg';
+import NotificationIcon from './NotificationIcon.svg';
+
+export { CartIcon, HomeIcon, NotificationIcon };
+
